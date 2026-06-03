670359ff6ac18b38af9e6d614b705ab680b0e2dc
toodiesjamming-stack <toodiesjamming@gmail.com>
2026-06-02 20:23:20 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -41,8 +41,8 @@ for _, folder in {'newvape', 'newvape/games', 'newvape/profiles', 'newvape/asset
 end
 
 if not shared.VapeDeveloper then
-	local _, subbed = pcall(function() 
-		return game:HttpGet('https://github.com/toodiesjamming-stack/Fuzzynuts') 
+	local _, subbed = pcall(function()
+		return game:HttpGet('https://github.com/toodiesjamming-stack/Fuzzynuts')
 	end)
 	local commit = subbed:find('currentOid')
 	commit = commit and subbed:sub(commit + 13, commit + 52) or nil
